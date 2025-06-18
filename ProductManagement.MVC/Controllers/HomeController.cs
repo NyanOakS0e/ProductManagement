9fa5a77d9907c6d3eddda43dafec33b1db4ae4c7
Nyan Oak Soe <nyanoaksoe77@gmail.com>
2025-06-18 12:34:11 +0630

    feat: bind MVC and API

--- ProductManagement.MVC/Controllers/HomeController.cs
+++ ProductManagement.MVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ProductManagement.MVC.Models;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace ProductManagement.MVC.Controllers
 {
@@ -51,7 +52,36 @@ namespace ProductManagement.MVC.Controllers
             return StatusCode((int)response.StatusCode, "Failed to delete.");
         }
 
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> UpdateAsync([FromBody] UpdateProductRequestModel reqModel)
+        {
+            var content = JsonContent.Create(reqModel);
+            var response = await _http.PostAsync("https://localhost:7297/api/Product/Update", content);
 
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok();
+            }
+
+            return StatusCode((int)response.StatusCode, "Failed to update.");
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+
+        public async Task<IActionResult> CreateAsync([FromBody] AddProductRequestModel reqModel)
+        {
+            var content = JsonContent.Create(reqModel);
+            var response = await _http.PostAsync("https://localhost:7297/api/Product/Create", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok();
+            }
+
+            return StatusCode((int)response.StatusCode, "Failed to Create.");
+        }
 
     }
 }
---
+++ ProductManagement.MVC/Models/UpdateProductRequestModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductManagement.MVC.Models
+{
+    public class UpdateProductRequestModel
+    {
+        public int ProductId { get; set; }
+
+        public string? Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+
+        public decimal? Price { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
